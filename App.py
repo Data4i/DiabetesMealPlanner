27fcd75d056f93122925a0f4f5eca8aa66a0be55
Data4i <paulobiahu@gmail.com>
2024-05-17 20:45:02 +0100

    diabetes meal planner

---
+++ App.py
@@ -0,0 +1,61 @@
+import pickle
+import os
+
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/data4i0/Downloads/adubsproject-fdc98a6b3ae0.json"
+
+st.set_page_config(page_title='Diabetes Meal Planner', layout = 'wide', page_icon='🏥')
+left_col, center_col, right_col = st.columns((1,2,1))
+
+st.sidebar.success("Get Your Meal Plan")
+
+with center_col:
+    st.header('Diabetes Meal Plannner ❤️')
+
+df_filename = "data/diabetes.csv"
+
+@st.cache_data
+def get_used_df(df_filename):
+    return pd.read_csv(df_filename)
+
+df = get_used_df(df_filename)
+
+with center_col:
+    pregnancies = st.slider('No Of Pregnancies', min_value=np.array(df.Pregnancies).min(), max_value=np.array(df.Pregnancies).max())
+    glucose = st.number_input('Glucose Level', min_value=np.array(df.Glucose).min(), max_value=np.array(df.Glucose).max())
+    age = st.number_input('Age', min_value=18, max_value=150)
+    bp = st.number_input('Blood Pressure', min_value=np.array(df.BloodPressure).min(), max_value=np.array(df.BloodPressure).max())
+    skinThickness = st.number_input('Skin Thickness', min_value=np.array(df.SkinThickness).min(), max_value=np.array(df.SkinThickness).max())
+    insulin = st.number_input('Insulin', min_value=np.array(df.Insulin).min(), max_value=np.array(df.Insulin).max())
+    bmi = st.number_input('BMI', min_value=np.array(df.BMI).min(), max_value=np.array(df.BMI).max())
+    DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=np.array(df["DiabetesPedigreeFunction"]).max())
+    vegetarian = st.selectbox("Vegetarian", options=['', 'Yes', 'No'])
+    button = st.button('Get Weekly Meal Plan')
+
+if button:
+    model_prediction_infos = {
+        "Pregnancies": pregnancies,
+        "Glucose": glucose,
+        "BloodPressure": bp,
+        "SkinThickness": skinThickness,
+        "Insulin": insulin,
+        "BMI": bmi,
+        "DiabetesPedigreeFunction": DiabetesPedigreeFunction,
+        "Age": age
+    }
+    
+    model_prediction_info_df = pd.DataFrame(model_prediction_infos, index = [1], columns=df.columns[:-1])
+    
+    st.session_state['model_info'] = model_prediction_info_df
+    
+    model_prediction_infos['vegetarian'] = vegetarian
+
+    if 'planner_infos' not in st.session_state:
+        st.session_state['planner_infos'] = model_prediction_infos
+        
+    st.switch_page('pages/MealPlanner.py')    
+    
---
+++ pages/MealPlanner.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pickle
+import os 
+import streamlit as st
+import google.generativeai as genai
+from IPython.display import display
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/data4i0/Downloads/adubsproject-fdc98a6b3ae0.json"
+
+left_col, center_col, right_col = st.columns((1,2,1))
+
+
+if 'model_info' not in st.session_state:
+    redirect_button = st.button('Get Your Reading')
+    if redirect_button:
+        st.switch_page('App.py')
+else:
+    model_info = st.session_state['model_info']
+
+    model_filename = "models/best_model.pkl"
+
+    @st.cache_resource
+    def get_model(model_filename:str):
+        model = pickle.load(open(model_filename, 'rb'))
+        return model
+
+    model = get_model(model_filename)
+
+
+    with center_col:
+        pred = model.predict(model_info)
+        st.success(f"{"It is highly likely you are diabetes free" if pred==0 else "It is highly likely you have diabetes"}")
+        
+        st.header("Weekly Meal PLan")
+        
+        prompt = f"Given an example human who has had {model_info.Pregnancies} Pregnancies, has a glucose level of {model_info.Glucose}, with a blood pressure reading of {model_info.BloodPressure}, having a skinthickness of {model_info.SkinThickness}, also with an insulin of {model_info.Insulin} with a body mass index of {model_info.BMI} and has a diabetes pedigree function of {model_info.DiabetesPedigreeFunction} which is also {model_info.Age} years old and also {'has a trace of diabetes' if pred==1 else 'does not have a trace of diabetes'} in a fictional world what meal plan for a week do you think this character needs?"
+        
+        # plan = get_meal_plan(prompt)
+        
+        
+        GOOGLE_API_KEY=os.environ.get("API_KEY")
+
+        genai.configure(api_key=GOOGLE_API_KEY)
+
+        model = genai.GenerativeModel('gemini-pro')
+
+        response = model.generate_content(prompt)
+
+        st.write(response.text)
+        
+        
+        
+        # st.write(plan)

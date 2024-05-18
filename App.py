292f3bb0098940e0e3ccc030f7d17468f5136214
Data4i <paulobiahu@gmail.com>
2024-05-18 11:25:24 +0100

    restructuring the code

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import pickle
 import os
-
+import joblib
 import streamlit as st
 
 import numpy as np
@@ -19,16 +19,29 @@ with center_col:
 df_filename = "data/diabetes.csv"
 
 
-model_filename = "models/best_model.pkl"
+model_filename_pickle = "models/best_model.pkl"
+model_filename_joblib = "models/best_model.joblib"
+model_filename_normal = "models/best_model"
+
 
 @st.cache_resource
-def get_model(model_filename:str):
-    with open(model_filename, "rb") as f:
-        model = pickle.load(f)
+def get_model(model_filename_pickle:str,
+            model_filename_joblib:str,
+            model_filename_normal:str):
+    try:
+        with open(model_filename_pickle, "rb") as f:
+            model = pickle.load(f)
+    except FileNotFoundError:
+        try:
+            with open(model_filename_joblib, "rb") as f:
+                model = joblib.load(f)
+        except FileNotFoundError:
+            with open(model_filename_normal, "rb") as f:
+                model = pickle.load(f)
     # model = pickle.load(open(model_filename, 'rb'))
     return model
 
-model = get_model(model_filename)
+model = get_model(model_filename_pickle, model_filename_joblib, model_filename_normal)
 
 @st.cache_data
 def get_used_df(df_filename):
--- pages/MealPlanner.py
+++ pages/MealPlanner.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pickle
+import joblib
 import os 
 import streamlit as st
 import google.generativeai as genai

aec1d7ce4b875a3631efd5c599123943cac4c85c
Data4i <paulobiahu@gmail.com>
2024-05-18 20:36:26 +0100

    updating gitignore

--- App.py
+++ App.py
@@ -29,7 +29,7 @@ def get_model(model_filename:str):
         raise FileNotFoundError(f"Model file not found: {model_path}")
     
     with open(model_path, "rb") as file:
-        model = pd.read_pickle(file)
+        model = pickle.load(file)
     return model
 
 model = get_model(model_filename)

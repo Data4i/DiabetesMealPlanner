df4ffffabf3937ba60306558227b4f302a766f8f
Data4i <paulobiahu@gmail.com>
2024-05-18 11:02:38 +0100

    restructuring the code

--- pages/MealPlanner.py
+++ pages/MealPlanner.py
@@ -15,6 +15,7 @@ if 'model_info' not in st.session_state:
         st.switch_page('App.py')
 else:
     isvegetarian = st.session_state['isvegetarian']
+    st.write(f"{isvegetarian}")
     model_info = st.session_state['model_info']
     pred = st.session_state['prediction']
 
@@ -24,7 +25,7 @@ else:
         
         st.header("Weekly Meal PLan")
         
-        prompt = f"Given an example human who has had {model_info.Pregnancies} Pregnancies, has a glucose level of {model_info.Glucose}, with a blood pressure reading of {model_info.BloodPressure}, having a skinthickness of {model_info.SkinThickness}, also with an insulin of {model_info.Insulin} with a body mass index of {model_info.BMI} and has a diabetes pedigree function of {model_info.DiabetesPedigreeFunction} which is also {model_info.Age} years old and also {'has a trace of diabetes' if pred==1 else 'does not have a trace of diabetes'} which is also a {'vegetarian' if isvegetarian==1 else 'not a vegetarian'} in a fictional world what meal plan for a week do you think this character needs?"
+        prompt = f"Given an example human who has had {model_info.Pregnancies} Pregnancies, has a glucose level of {model_info.Glucose}, with a blood pressure reading of {model_info.BloodPressure}, having a skinthickness of {model_info.SkinThickness}, also with an insulin of {model_info.Insulin} with a body mass index of {model_info.BMI} and has a diabetes pedigree function of {model_info.DiabetesPedigreeFunction} which is also {model_info.Age} years old and also {'has a trace of diabetes' if pred==1 else 'does not have a trace of diabetes'} which is also a {'vegetarian' if isvegetarian=='Yes' else 'not a vegetarian'} in a fictional world what meal plan for a week do you think this character needs?"
         
         # plan = get_meal_plan(prompt)
         
